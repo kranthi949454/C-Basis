115aaa487f3d45ead3cfc039251ed73e271d9b1e
K.Kranthi Durga Prasad <93141669+kranthi949454@users.noreply.github.com>
2022-04-01 10:36:48 +0530

    Add files via upload

---
+++ Amstrong number.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()	
+{
+	int n,sum=0,r,temp;
+	scanf("%d", &n);
+	temp=n;
+	while(n!=0)
+	{
+		r=n%10;
+		sum=sum+(r*r*r);
+		n=n/10;
+	}
+	if (temp==sum)
+	printf("Amstrong Number");
+	else
+	printf("Not an Amstrong Number");
+}
